a617c17c31fc7d9e5f25102bcf5a4bfaef6d27a4
analeticiarc <152559893+analeticiarc@users.noreply.github.com>
2023-11-30 23:45:08 +0000

    (feat):

---
+++ teste.py
@@ -0,0 +1,16 @@
+def sum_numbers(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+        a (float): the first term
+        b (float): the second term
+
+    Returns:
+        float: sum of both arguments
+    """
+   
+    num = a + b
+
+    return num
+
+soma(29,4)
