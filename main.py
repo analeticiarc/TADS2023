40837472a9d8ab365309fffe00e3aeb4ab345940
analeticiarc <152559893+analeticiarc@users.noreply.github.com>
2024-01-04 10:42:52 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def sum_numbers(a: float, b: float) -> float:
+    """ Sum two numbers
+
+    Args:
+        a (float): first term
+        b (float): second term
+
+    Returns:
+        float: return the sum
+    """
+    return a + b
